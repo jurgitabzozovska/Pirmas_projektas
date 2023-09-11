2421499c095b9990dfea4606bd09e229ba96bddd
jurgitabzozovska <144364932+jurgitabzozovska@users.noreply.github.com>
2023-09-11 20:33:32 +0300

    initial commit

--- main.py
+++ main.py
@@ -682,8 +682,148 @@ Tekstas = 'Siandien yra sekmadienis ir as turiu daryti namu darbus'
 # def mano_sarasas(sarasas):
 #    sandauga = 1
 #    for skaicius in sarasas:
-#         sandauga = sandauga * skaicius
+#         sandauga *= skaicius
 #    return sandauga
 # sk_sarasas = [2, 4, 6, 8, 10]
 # print('Saraso skaiciu sandauga lygi:',mano_sarasas (sk_sarasas))
+
+# 09-11 PAMOKA OBJEKTINIS PROGRAMAVIMAS
+# ***********************1. UZDUOTIS
+##     sukuriama klase
+# class Zmogus:
+
+##     sukuriamas konstruktorius
+#     def __init__(self, vardas, amzius):
+#         self.vardas = vardas
+#         self.amzius = amzius
+
+##      sukuriami metodai
+#     def sveikinimas(self):
+#         return f"Sveiki, as esu {self.vardas} ir man yra {self.amzius} metu"
+
+
+# #     sukuriamas objektas
+## zmogus1 = Zmogus("Migle", 30)
+# zmogus2 = Zmogus("Antanas", 45)
+# print(zmogus1.sveikinimas())
+# print(zmogus2.sveikinimas())
+
+
+# ***********************2. UZDUOTIS
+# class Automobilis:
+#     def __init__(self, marke, modelis):
+#         self.marke = marke
+#         self.modelis = modelis
+#         self.greitis = 0
+
+##     def akseleratorius(self):
+#         self.greitis += 10
+
+##     def stabdis(self):
+#         self.greitis -= 5
+
+##     def info(self):
+#         return f'{self.marke} {self.modelis}, greitis: {self.greitis} km/h'
+
+## auto1=Automobilis('Mazda', '323')
+# auto1.akseleratorius()
+# auto1.akseleratorius()
+# auto1.akseleratorius()
+# auto1.stabdis()
+# print(auto1.info())
+
+# ***********************3. UZDUOTIS
+# class Knyga:
+#     def __init__(self, pavadinimas, autorius, leidimo_metai):
+#         self.pavadinimas = pavadinimas
+#         self.autorius = autorius
+#         self.leidimo_metai = leidimo_metai
+#
+#     def info(self):
+#         return f'Knygos pavadinimas: {self.pavadinimas}, autorius: {self.autorius}, leidimo metai: {self.leidimo_metai}'
+# knyga1 = Knyga('Saule', 'Jurgita', '2023')
+# print(knyga1.info())
+
+#
+# ***********************4. UZDUOTIS
+# class Preke:
+#     def __init__(self, pavadinimas, kaina):
+#         self.pavadinimas = pavadinimas
+#         self.kaina = kaina
+#     def info(self):
+#         return f'{self.pavadinimas}: {self.kaina} eurų'
+#
+# class Krepselis:
+#     def __init__(self):
+#        self.prekes = []
+#    def ideti_preke(self, preke):
+#         self.prekes.append(preke)
+#     def krepselio_info(self):
+#         if not self.prekes:
+#             print("tokios prekes nera")
+#         else:
+#             print("krepselio turinys: ")
+#             for preke in self.prekes:
+#                 print(preke.info())
+#     def bendra_suma(self):
+#         suma = sum(preke.kaina for preke in self.prekes)
+#         return suma
+#
+# krepsys = Krepselis()
+# preke1 = Preke('obuolis', 5.0)
+# preke2 = Preke('bananas', 2.2)
+# preke3 = Preke('vanduo', 1.3)
+# preke4 = Preke('kava', 10.5)
+#
+# krepsys.ideti_preke(preke1)
+# krepsys.ideti_preke(preke2)
+# krepsys.ideti_preke(preke3)
+# krepsys.ideti_preke(preke4)
+#
+# krepsys.krepselio_info()
+#
+# print(f'Bendra suma: {krepsys.bendra_suma()} eurų')
+
+# ***********************5. UZDUOTIS
+# Sukurkite klasę "Saskaita", kuri turėtų šias savybes ir metodus:
+#
+#     * saskaitos_nr: sąskaitos numeris.
+#     * balansas: sąskaitos balansas (numatytasis pradžioje yra 0).
+#     * inesti(suma): metodas, kuris prideda nurodytą sumą prie sąskaitos balanso.
+#     * isimti(suma): metodas, kuris sumažina sąskaitos balansą nurodyta suma,
+# jei yra pakankamai lėšų, arba išveda pranešimą apie nepakankamą balansą.
+#     * informacija(): metodas, kuris grąžina informaciją apie sąskaitą (sąskaitos numeris ir balansas).
+#
+# Sukurkite bent du objektus pagal šią klasę ir atlikite operacijas, tokiu kaip lėšų įnešimas ir išėmimas, bei gaukite sąskaitos informaciją.
+
+
+# ***********************6. UZDUOTIS
+# Sukurkite klasę "Studentas", kuri turėtų šias savybes:
+# #     * vardas: studento vardas.
+#     * pazymiai: sąrašas su studento pažymiais.
+# # Sukurkite klasę "Mokytojas", kuri turėtų šias savybes:
+# #     * vardas: mokytojo vardas.
+#     * Mokytojo dėstoma tema: mokytojo dėstomas dalykas.
+# # Papildykite "Studentas" klasę metodu vidurkis(), kuris apskaičiuoja
+# studento pažymių vidurkį.
+# # Papildykite "Mokytojas" klasę metodu ivertinimas(studentas, pazymys),
+# kuris prideda studentui pažymį.
+# # Sukurkite objektus pagal "Studentas" ir "Mokytojas" klases,
+# pridėkite pažymius ir vykdykite vidurkio apskaičiavimus bei pažymių pridėjimus.
+
+# class Studentas:
+#     def __init__(self, st_vardas, pazymiai):
+#         self.st_vardas = st_vardas
+#         self.pazymiai = pazymiai
+#     def st_vidurkis(self):
+#         self
+#
+#
+# class Mokytojas:
+#     def __init__(self, mokytojo_vardas, destomas_dakykas):
+#         self.mokytojo_vardas = mokytojo_vardas
+#         self.destomas_dalykas = destomas_dakykas
+#     def ivertinimas(self, pazymys):
+
+
 
