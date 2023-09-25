1fc84f62e9a814b0b31dbd0c970b441ae3fc8726
jurgitabzozovska <144364932+jurgitabzozovska@users.noreply.github.com>
2023-09-25 15:50:00 +0300

    initial commit

--- WEB Svraping_mano bandymai.py
+++ WEB Svraping_mano bandymai.py
@@ -37,29 +37,47 @@ import psycopg2
 
 
 
-url = "https://vaga.lt/naujos-knygos"
-responce = requests.get(url)
-# print(responce)
-soup = BeautifulSoup(responce.text, "html.parser")
-Pavadinimai = soup.find_all("p", class_="name")
-Knygos_pavadinimas = []
-for i in Pavadinimai:
-    pavadinimas = i.text
-    Knygos_pavadinimas.append(pavadinimas)
-# print(Knygos_pavadinimas)
-Autoriai = soup.find_all("p", class_="Autorius")
-Autoriaus_vardas_pavarde = []
-for i in Autoriai:
-    autorius = i.text
-    Autoriaus_vardas_pavarde.append(autorius)
-# print(Autoriaus_vardas_pavarde)
-Kaina = soup.find_all("div", class_="price price-align-wrapper")
-Kainos = []
-for i in Kaina:
-    kaina = i.text
-    Kainos.append(kaina)
-# print(Kainos)
-df = pd.DataFrame({"Knygos pavadinimas":Knygos_pavadinimas, "Autorius": Autoriaus_vardas_pavarde, "Kaina": Kainos})
-# print(df)
-df.to_csv("knygos.csv")
+# url = "https://vaga.lt/naujos-knygos"
+# responce = requests.get(url)
+# # print(responce)
+# soup = BeautifulSoup(responce.text, "html.parser")
+# Pavadinimai = soup.find_all("p", class_="name")
+# Knygos_pavadinimas = []
+# for i in Pavadinimai:
+#     pavadinimas = i.text
+#     Knygos_pavadinimas.append(pavadinimas)
+# # print(Knygos_pavadinimas)
+# Autoriai = soup.find_all("p", class_="Autorius")
+# Autoriaus_vardas_pavarde = []
+# for i in Autoriai:
+#     autorius = i.text
+#     Autoriaus_vardas_pavarde.append(autorius)
+# # print(Autoriaus_vardas_pavarde)
+# Kaina = soup.find_all("div", class_="price price-align-wrapper")
+# Kainos = []
+# for i in Kaina:
+#     kaina = i.text
+#     Kainos.append(kaina)
+# # print(Kainos)
+# df = pd.DataFrame({"Knygos pavadinimas":Knygos_pavadinimas, "Autorius": Autoriaus_vardas_pavarde, "Kaina": Kainos})
+# # print(df)
+# df.to_csv("knygos.csv")
+
+
+# url = "https://www.worldometers.info/world-population/population-by-country/"
+# responce = requests.get(url)
+# # print(responce)
+# soup = BeautifulSoup (responce.text, "html.parser")
+# # print(soup)
+# rows = soup.find("table", {"id": "example2"}).find("tbody"). find_all("tr")
+# Countries_list = []
+# for row in rows:
+#     dic = {}
+#     dic["Country"] = row.find_all("td")[1].text
+#     dic["Popoliacija"] = row.find_all("td")[2].text.replace(",", " ")
+#     Countries_list.append(dic)
+# # print(Countries_list)
+# df = pd.DataFrame (Countries_list)
+# df.to_csv("Salys.csv", index=False)
+
 
