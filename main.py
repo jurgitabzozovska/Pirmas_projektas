0a3df05c2cbc6b500abe75c6932a9caee083ee95
DianaMokslai <diana.stre@gmail.com>
2023-10-09 18:42:23 +0300

    2023-10-09

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+from bs4 import BeautifulSoup
+import requests
+import numpy as np
+import seaborn as sns
+
+csv_failo_pavadinimas = 'eismo ivykiai.csv'
+df = pd.read_csv(csv_failo_pavadinimas)
+print(df)
+
+df = pd.DataFrame(df)
+
+
+
