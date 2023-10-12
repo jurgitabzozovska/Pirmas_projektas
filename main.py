713ba6087f8d60c8b8b36a13323822245929af75
DianaMokslai <diana.stre@gmail.com>
2023-10-12 21:56:08 +0300

    2023-10-12

--- main.py
+++ main.py
@@ -12,12 +12,13 @@ import seaborn as sns
 # csv_failo_pavadinimas = 'eismo ivykiai.csv'
 # data1 = pd.read_csv(csv_failo_pavadinimas)
 # # # print(df)
-# # # #
+
 # df1 = pd.DataFrame(data1)
 # df1[['Metai', 'Menuo']] = df1["Laikotarpis"].str.split('M', expand=True)
 # df1 = df1.drop(['Laikotarpis','Rodiklis','Matavimo vienetai'], axis=1)
 # df1.to_csv('eismo_ivykiai.csv', index=False)
 # # # print(df1)
+
 # Suminis_grupavimas = df1.groupby('Metai')['Reikšmė'].sum()
 # # print(Suminis_grupavimas)
 # # # vid pagal metus
@@ -56,34 +57,37 @@ import seaborn as sns
 # plt.title('Kelių eismo įvykiai ir jų vidurkis pagal metus')
 # plt.xticks(Metai,  Metai)
 # plt.show()
-#
 
-# linijine
-# csv_failo_pavadinimas = 'neblaiviu asmenu kalte pagal apsk.csv'
-# data2 = pd.read_csv(csv_failo_pavadinimas)
-# # print(data2)
-# df2 = pd.DataFrame(data2)
-# #sujungimas dviejų csv failų
-# sujungimas = pd.concat([df1, df2],ignore_index =True)
-# # print(sujungimas)
-# sujungimas.dropna()
-#
-# sujungimas.to_csv('sujungimas.csv', index=False)
-# print("CSV file sukurtas")
 
-#
-#
-#
-# # # #
-# 2csv_failo_pavadinimas = 'Lytis.csv'
+#2. neblaiviu ivykiai pagal metus nuo min iki max
+# csv_failo_pavadinimas = 'neblaiviu_ivykiai_pagal menesius_metus.csv'
+# data3 = pd.read_csv(csv_failo_pavadinimas)
+# # print(data3)
+# df3 = pd.DataFrame(data3)
+# df3[['Metai', 'Menuo']] = df3["Laikotarpis"].str.split('M', expand=True)
+# # print(df3)
+# df3 = df3.drop(['Laikotarpis'], axis=1)
+# # print(df3)
+# x = df3['Metai']
+# y = df3['Reikšmė']
+# plt.plot(x, y, label = 'įvykiai', color = 'pink', linestyle = '', marker = 'o')
+# plt.legend()
+# plt.xlabel('Metai')
+# plt.ylabel('Įvykių skaičius')
+# plt.title('Kelių eismo įvykiai, kuriuose nukentėjo žmonės dėl neblaivių asmenų')
+# plt.show()
+
+
+#3 ivykiai pagal lyti ir amziu
+# csv_failo_pavadinimas = 'Lytis.csv'
 # data2 = pd.read_csv(csv_failo_pavadinimas)
 # print(data2)
 # df2 = pd.DataFrame(data2)
 # print(df2)
-# #
+
 # Suminis_grupavimas = df2.groupby(["Amžius", 'Lytis'])['Reikšmė'].sum()
-# # # print(Suminis_grupavimas)
-# #
+# # # print(Suminis_grupavimas)\
+
 # moteru_grupavimas = df2[df2['Lytis'] == 'Moterys']
 # # # print(moteru_grupavimas)
 # M_group = moteru_grupavimas.groupby(['Amžius'])['Reikšmė'].sum().astype(int)
@@ -105,8 +109,7 @@ import seaborn as sns
 # total_V = V_group.values
 # print(total_V)
 #
-# age = ['0-6','6–9','10–14','15–17','18–20','21–24','25–34','35–44','45–54','55–64',
-# '65+']
+# age = ['0-6','6–9','10–14','15–17','18–20','21–24','25–34','35–44','45–54','55–64','65+']
 # Male = [130, 162, 278, 385, 364, 436, 1118, 895, 699, 630, 527]
 # Female = [92, 133, 234, 203, 231, 312, 551, 518, 585, 643, 948]
 #
@@ -127,146 +130,50 @@ import seaborn as sns
 # plt.barh(y=population_df["Age"], width=population_df["Male_Width"], left=population_df["Male_Left"],
 #         color="#4682b4", label="Male");
 # # plt.show()
-# #
-# # plt.xticks(1200, "Male", fontsize=25, fontweight="bold");
-# # plt.xticks(1200,  "Female", fontsize=25, fontweight="bold");
-# plt.xticks(range(-1200,1201,100));
-# plt.show()
-# # #
-# #
+#
+# plt.xticks(range(-1200,1201,250));
 # plt.xlabel('sužeistųjų ir žuvusiųjų skaičius',fontsize=10, fontweight="bold")
 # plt.ylabel('Amžius',fontsize=10, fontweight="bold")
 # plt.title("Kelių eismo įvykiuose sužeistųjų ir žuvusiųjų skaičius pagal amžių",loc="left", pad=20, fontsize=25, fontweight="bold")
 # plt.xlim(-1200,1200);
 #
-# plt.xticks(range(-1200,1201), ["{} %". format(i) for i in range(-1200,1201)]);
-# plt.legend(loc="best");
-#
-# plt.show()
 # plt.text(-5, 17, "Male", fontsize=25, fontweight="bold");
 # plt.text(4, 17, "Female", fontsize=25, fontweight="bold");
-#
-# for idx in range(len(population_df)):
-#     plt.text(x=population_df["Male_Left"][idx]-0.1, y=idx, s="{} %".format(population_df["Male"][idx]),
-#             ha="right", va="center",
-#             fontsize=15, color="#4682b4");
-#     plt.text(x=population_df["Female_Width"][idx]+0.1, y=idx, s="{} %".format(population_df["Female"][idx]),
-#             ha="left", va="center",
-#             fontsize=15, color="#ee7a87");
-#
-# plt.xlim(-15,15);
-# plt.xticks(range(-15,16), ["{} %".format(i) for i in range(-15,16)]);
-#
-# plt.legend(loc="best");
-#
-# plt.xlabel("Percent (%)", fontsize=16, fontweight="bold")
-# plt.ylabel("Age Range", fontsize=16, fontweight="bold")
-# plt.title("Eismo įvykiai pagal amžių ir lytį", loc="left", pad=20, fontsize=25, fontweight="bold")
 # plt.show()
 
 
-
-csv_failo_pavadinimas = 'Pagal transporto priemones.csv'
-data1 = pd.read_csv(csv_failo_pavadinimas)
-print(data1)
-
-grupuojame = data1.groupby(['Nukentėjusiųjų tipas','Kelių naudotojai'])['Reikšmė'].sum().astype(int)
-print(grupuojame)
-print(len(grupuojame))
-print(type(grupuojame))
-
-first_table = grupuojame.loc['Sužeistieji']
-print(first_table)
-second_table = grupuojame.loc['Žuvusieji']
-print(second_table)
-
-# define Seaborn color palette to use
-palette_color = sns.color_palette('dark')
-
-plt.figure(figsize=(10, 10))
-plt.subplot(1,2,1)
-plt.pie(first_table, colors=palette_color, autopct='%.0f%%')
-plt.title('Kelių naudotojai')
-plt.subplot(1,2,2)
-plt.pie(second_table, colors=palette_color, autopct='%.0f%%')
-plt.title('Kelių naudotojai2')
-plt.legend(loc='upper left', labels=first_table.index, borderpad=0, bbox_to_anchor=(0.1, 0.1))
-
-
-
-plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# df2['Amžius'] = df2['Amžius'].replace('iki', '0')
-# print(df2['Amžius'])
-# print(type)
-
-# #pagal lytis grupavimas
-# grupavimas = df2.groupby(['Laikotarpis','Lytis'])['Reikšmė'].sum()
-# # print(grupavimas)
-# #pagal amžių
-
+# #4suzeistuju ir zuvusiuju skaicius per 3 metus pagal dalyvius
+# csv_failo_pavadinimas = 'Pagal transporto priemones.csv'
+# data1 = pd.read_csv(csv_failo_pavadinimas)
+# print(data1)
 #
-
-
-# Amzius = df2.groupby(['Amžius','Rodiklis','Lytis'])['Reikšmė'].sum().astype(int)
-# print(Amzius)
-# Amžius = df2['Amžius']
+# grupuojame = data1.groupby(['Nukentėjusiųjų tipas','Kelių naudotojai'])['Reikšmė'].sum().astype(int)
+# print(grupuojame)
+# print(len(grupuojame))
+# print(type(grupuojame))
 #
-# Moterys = df2[df2['Lytis'] == "Moterys"]
-# Vyrai = df2[df2["Lytis"] == 'Vyrai']
-# moterys_grupavimas = Moterys.groupby('Amžius)['Reikšmė'].sum().astype(int)
+# first_table = grupuojame.loc['Sužeistieji']
+# print(first_table)
+# second_table = grupuojame.loc['Žuvusieji']
+# print(second_table)
 #
+# # define Seaborn color palette to use
+# palette_color = sns.color_palette('colorblind')
+# ex=(0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1)
+# plt.figure(figsize=(12,12))
+# plt.subplot(1,2,1)
+# plt.pie(first_table, colors=palette_color, autopct='%.0f%%', explode = ex)
+# # plt.title('Kelių eismo įvykiuose sužeistųjų skaičius',fontsize=12)
+# #
+# # plt.subplot(1,2,2)
+# # plt.pie(second_table, colors=palette_color, autopct='%.0f%%',explode = ex)
+# # plt.title('Kelių eismo įvykiuose žuvusiųjų skaičius',fontsize = 12)
+# # plt.legend(loc='upper center', labels=first_table.index, borderpad=0, bbox_to_anchor=(0.1, 0.1))
+# # plt.show()
+# #
+# #
 #
-#
-# df2 = pd.DataFrame({"Age": [Amžius], "Male": [Vyrai], "Female": [Moterys]})
-#
-# print(df2)
-#
-# bar_plot = sns.barplot(x='Vyrai', y='Amžius', data=df2, order='Reikšmė', palette='OrRd', lw=0)
-# bar_plot = sns.barplot(x='Moterys', y='Amžius', data=df2, order='Reikšmė', palette='PuBu', lw=0)
-# print(bar_plot)
-
-# bar_plot.set(xlabel="Population by sex", ylabel="Age-Group", title = "1980")
-# bar_plot.set_xticklabels(labels)
-
-
-
-# df2["Female_Left"] = 0
-# df2["Female_Width"] = df2["Moterys"]
-#
-# df2["Male_Left"] = -df2["Vyrai"]
-# df2["Male_Width"] = df2["Vyrai"]
-#
-# print(df2)
-
-#
-# Apskritis = df2.groupby(['Laikotarpis','Administracinė teritorija'])['Reikšmė'].sum()
-# print(Apskritis)
-
-#per 3 metus
-# Apskritis2 = df2.groupby(['Administracinė teritorija'])['Reikšmė'].sum()
-# print(Apskritis2)
-# # #
-
-#
+# #
 # # # # Latvijos žuvusiųjų žmonių skaičius
 # url = 'https://www.csdd.lv/en/road-accidents/the-road-traffic-safety-statistics'
 # response = requests.get(url)
@@ -282,7 +189,7 @@ plt.show()
 #
 #     columns = row.find_all('td')
 #     year = columns[0].text.strip()
-#     from_date_columns = columns[1:2]
+#     from_date_columns = columns[2:]
 #     from_date = ' '.join([col.text.strip() for col in from_date_columns])
 #     from_date_numbers = from_date.split()
 #
@@ -293,7 +200,8 @@ plt.show()
 #
 # df2 = pd.DataFrame(data, columns=titles)
 # print(df2)
-# # df2.drop('From')
+# df2 = df2.drop(0)
+# print(df2)
 # df2.to_csv('Latvija.csv', index=False)
 # print("CSV file sukurtas")
 
@@ -302,15 +210,6 @@ plt.show()
 # df3 = pd.read_csv('estijos ivykiai.csv')
 # # print(df3)
 # df3 = pd.DataFrame(df3)
-# df3[['ivykiai','menuo']] = df3["Indicator"].str.split(' ', expand=True)
+# # print(df3)
+# df3 = df3.drop(0)
 # print(df3)
-
-
-
-
-# #sujungimas tris csv failus
-# sujungimas = pd.concat([df1, df2, df3], ignore_index=True,)
-# print(sujungimas)
-# sujungimas.to_csv('sujungimas.csv', index=False)
-# print("CSV file sukurtas")
-#
--- map.py
+++ map.py
@@ -5,23 +5,48 @@ import requests
 import numpy as np
 import seaborn as sns
 import folium
-m = folium.Map(location =[55.74522103685154, 21.115998695107294], zoom_start = 9)
+m = folium.Map()
+# (location =[55.74522103685154, 21.115998695107294], zoom_start=9))
 
 
 tooltip = 'Klaipedos_apskritis'
 tooltip_2 = 'Siauliu_apskritis'
+tooltip_3 = 'Kauno_apskritis'
+tooltip_4 = 'Alytaus_apskritis'
+tooltip_5 = 'Marijampolės_apskritis'
+tooltip_6 = 'Panevėžio_apskritis'
+tooltip_7 = 'Tauragės_apskritis'
+tooltip_8 = 'Telšių_apskritis'
+tooltip_9 = 'Utenos_apskritis'
+tooltip_10 = 'Vilniaus_apskritis'
 
-folium.Marker([55.74522103685154, 21.115998695107294], popup ='Klaipedos_apskritis', tooltip=tooltip).add_to(m)
-folium.Marker([55.93541715494644, 23.314281451279456], popup ='Siauliu_apskritis', tooltip=tooltip).add_to(m)
+
+folium.Marker([55.85769555597272, 21.201016443516764], popup ='Klaipedos_apskritis', tooltip=tooltip).add_to(m)
+folium.Marker([55.93541715494644, 23.314281451279456], popup ='Siauliu_apskritis', tooltip=tooltip_2).add_to(m)
+folium.Marker([54.987061474233016, 23.95325788817995], popup = 'Kauno_apskritis', tooltip = tooltip_3).add_to(m)
+folium.Marker([54.29787234367851, 24.08204682928195], popup = 'Alytaus_apskritis', tooltip = tooltip_4).add_to(m)
+folium.Marker([54.55393386606704, 23.354531619269398], popup = 'Marijampolės_apskritis', tooltip = tooltip_5).add_to(m)
+folium.Marker([55.9751616402609, 25.0798177183858], popup = 'Panevežio_apskritis', tooltip = tooltip_6).add_to(m)
+folium.Marker([55.24825227738376, 22.29501453149007], popup = 'Tauragės_apskritis', tooltip = tooltip_7).add_to(m)
+folium.Marker([55.9790078321016, 22.25038452365091], popup = 'Telšių_apskritis', tooltip = tooltip_8).add_to(m)
+folium.Marker([55.49987587972921, 25.60791218551143], popup = 'Utenos_apskritis', tooltip = tooltip_9).add_to(m)
+folium.Marker([54.808622296543824, 25.217868210446827], popup = 'Vilnius_apskritis', tooltip = tooltip_10).add_to(m)
+
 
 m.save('name.html')
 #55.74522103685154, 21.115998695107294
 #55.93541715494644, 23.314281451279456
+# 54.899226753963404, 23.90700517624643
+# 54.40144587269272, 24.0402348014765
+# 54.55393386606704, 23.354531619269398
+# 55.73413071383902, 24.36117277569358
+# 55.24825227738376, 22.29501453149007
+# 55.9790078321016, 22.250384523650915
+# 55.49987587972921, 25.60791218551143
+# 55.93189532554277, 23.310371928321867
 
-
-#
-# df2 = pd.read_csv('Apskritis.csv')
-# # print(df2)
-# df2 = pd.DataFrame(df2)
-# Apskritis = df2.groupby(['Administracinė teritorija'])['Reikšmė'].sum().astype(int)
-# # print(Apskritis)
+df2 = pd.read_csv('Apskritis.csv')
+# print(df2)
+df2 = pd.DataFrame(df2)
+Apskritis = df2.groupby(['Administracinė teritorija'])['Reikšmė'].sum().astype(int)
+print(Apskritis)
